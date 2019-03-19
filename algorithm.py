3176681c63497bc41f7328f446f9b54825b27cbe
landjbs <landonsmith@college.harvard.edu>
2019-03-19 17:19:26 -0600

    class init

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
+class Animal:
+    def __init__(self, number, fear, hunger, curiosity):
+        self.number = number
+        self.fear = fear
+        self.hunger = hunger
+        self.curiosity = curiosity
+
+p1 = Animal(number=10, hunger= np.random.normal(0,1, 10), fear=1, curiosity=1)
+
+print(p1.number)
+
+plt.hist(p1.hunger)
+plt.show()
+
+# import pandas as pd
+#
 # genome is created as blank slate {"hunger":0, "curiosity":0, "fear":0}
-genome = [1,1,1]
-
-genomeList = [genome, list(map((lambda x : x * 2), genome))]
-
-print(genomeList)
-
-
-def sim_step(genomeList, repRate):
-    newList = []
-    for creature in genomeList:
-        newList.append()
+# genome = [1,1,1]
+#
+# genomeList = [genome, list(map((lambda x : x * 2), genome))]
+#
+# print(genomeList)
+#
+#
+# def sim_step(genomeList, repRate):
+#     newList = []
+#     for creature in genomeList:
+#         newList.append()
